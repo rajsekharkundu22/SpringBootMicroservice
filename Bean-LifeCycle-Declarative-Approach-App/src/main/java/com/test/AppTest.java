a0e501b29556144a81525be9ccbf73b1aaa15011
rajsekharkundu22 <103489563+rajsekharkundu22@users.noreply.github.com>
2023-09-02 15:16:25 +0530

    Add files via upload

---
+++ Bean-LifeCycle-Declarative-Approach-App/src/main/java/com/test/AppTest.java
@@ -0,0 +1,20 @@
+package com.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AppTest {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("Bean.xml");
+		
+		UserDao dao = context.getBean(UserDao.class);
+		dao.getData();
+		
+		//For stop IOC container
+		ConfigurableApplicationContext ccontext = (ConfigurableApplicationContext) context;
+		ccontext.close();
+	}
+
+}
---
+++ Bean-LifeCycle-Declarative-Approach-App/src/main/java/com/test/UserDao.java
@@ -0,0 +1,16 @@
+package com.test;
+
+public class UserDao {
+
+	public void init() {
+		System.out.println("getting db connection");
+	}
+	
+	public void getData() {
+		System.out.println("getting data from database");
+	}
+	
+	public void destroy() {
+		System.out.println("closing database conncetion");
+	}
+}
